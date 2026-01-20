811d5ae10abfb0e5ee2e66156b64c3d76d8163c4
baimo <1516105681@qq.com>
2026-01-20 16:10:34 +0800

    加了报错宏定义

--- chapter_2/vec_add.cu
+++ chapter_2/vec_add.cu
@@ -1,65 +1,69 @@
-#include<stdio.h>
-#include<cuda_runtime.h>
-#include<malloc.h>
-#include<iostream>
+#include <cuda_runtime.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-__global__ void vec_add_kernel(float *A_d,float *B_d, float *C_d,int n){
-    int i = threadIdx.x + blockDim.x * blockIdx.x;
-
-    if(i<n){
-        A_d[i] = B_d[i] + C_d[i];
+#define CHECK_CUDA_ERROR(val) check((val), #val, __FILE__, __LINE__)
+template <typename T>
+void check(T err, const char* const func, const char* const file, const int line)
+{
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "CUDA Runtime Error at: %s: %d\n", file, line);
+        fprintf(stderr, "%s %s\n", func, cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
     }
 }
 
-void vec_add(float *A_h,float *B_h, float *C_h, int n){
-    //申请显存空间
-    float *A_d, *B_d,*C_d;
-    cudaMalloc((void **)&A_d, sizeof(float) * n);
-    cudaMalloc((void **)&B_d, sizeof(float) * n);
-    cudaMalloc((void **)&C_d, sizeof(float) * n);
-
-    //把内存搬去显存
-    cudaMemcpy(B_d, B_h, sizeof(float) * n, cudaMemcpyHostToDevice);
-    cudaMemcpy(C_d, C_h, sizeof(float) * n, cudaMemcpyHostToDevice);
-
-    //调用核函数
-    int blockSize = 256;
-    // (n + 256 - 1) / 256 确保向上取整
-    int gridSize = (n + blockSize - 1) / blockSize;
-    vec_add_kernel<<<gridSize, blockSize>>>(A_d, B_d, C_d, n);
-
-    //把运算结果搬回来
-    cudaMemcpy(A_h, A_d, sizeof(float) * n, cudaMemcpyDeviceToHost);
-    // 记得释放显存
-    cudaFree(A_d);
-    cudaFree(B_d);
-    cudaFree(C_d);
+__global__ void vec_add(float *A, float *B, float *C, int n) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) {
+        C[i] = A[i] + B[i]; 
+    }
 }
 
-int main(){
-    // 主机参数申请空间
-    float *A_h,*B_h,*C_h;
-    int n = 1000;
-    int size_float = sizeof(float);
-    A_h = (float*)malloc(size_float * n);
-    B_h = (float*)malloc(size_float * n);
-    C_h = (float*)malloc(size_float * n);
+int main() {
+    int n = 1000; 
+    size_t bytes = n * sizeof(float);
+
+    vector<float> h_A(n);
+    vector<float> h_B(n);
+    vector<float> h_C(n);
 
     for (int i = 0; i < n; i++) {
-        B_h[i] = i;       
-        C_h[i] = i * 2;   
+        h_A[i] = i;
+        h_B[i] = i * 2;
     }
 
-    vec_add(A_h, B_h, C_h,n);
+    float *d_A, *d_B, *d_C;
+    CHECK_CUDA_ERROR(cudaMalloc(&d_A, bytes));
+    CHECK_CUDA_ERROR(cudaMalloc(&d_B, bytes));
+    CHECK_CUDA_ERROR(cudaMalloc(&d_C, bytes));
 
-    // 验证前10个数
-    for (int i = 0; i < 10; i++) {
-        cout << i << ": " << B_h[i] << " + " << C_h[i] << " = " << A_h[i] << endl;
+    CHECK_CUDA_ERROR(cudaMemcpy(d_A, h_A.data(), bytes, cudaMemcpyHostToDevice));
+    CHECK_CUDA_ERROR(cudaMemcpy(d_B, h_B.data(), bytes, cudaMemcpyHostToDevice));
+
+    int blockSize = 256;
+    int gridSize = (n + blockSize - 1) / blockSize;
+    
+    vec_add<<<gridSize, blockSize>>>(d_A, d_B, d_C, n);
+    
+    // 检查核函数是否启动报错
+    CHECK_CUDA_ERROR(cudaGetLastError());
+    // 等待 GPU 做完
+    CHECK_CUDA_ERROR(cudaDeviceSynchronize());
+
+    CHECK_CUDA_ERROR(cudaMemcpy(h_C.data(), d_C, bytes, cudaMemcpyDeviceToHost));
+
+    printf("Result verification:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("%.1f + %.1f = %.1f\n", h_A[i], h_B[i], h_C[i]);
     }
 
-    free(A_h);
-    free(B_h);
-    free(C_h);
+    CHECK_CUDA_ERROR(cudaFree(d_A));
+    CHECK_CUDA_ERROR(cudaFree(d_B));
+    CHECK_CUDA_ERROR(cudaFree(d_C));
+
+    return 0;
 }
